f564f63c1cd7e42bf2294cb6569d1aa0c415eca6
Alfie Collins <alfiecollins44@hotmail.co.uk>
2023-02-04 20:37:34 +0000

    adding an error page

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -5,6 +5,8 @@ const recipeSchema = new Schema({
   name: String,
   instructions: String,
   imgUrl: { type: String, default: "../images/cutlerynoback.png" },
+  isVegetarian: Boolean,
+  isVegan: Boolean,
 });
 
 module.exports = model("Recipe", recipeSchema);
